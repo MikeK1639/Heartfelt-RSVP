ff1c705dc8c40df5c0348f939f6029794590a9e1
robles1999 <30624599+robles1999@users.noreply.github.com>
2023-05-11 08:43:14 -0400

    Corrected RSVP route.

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -67,6 +67,7 @@ router.post("/guest", async (req, res) => {
       return res.status(400).send("Guest name is required."); // Return an error if the name is missing
     }
     await heartfelt_db.addGuest(guestData.guest_name); // Add the guest name to the database
+    
     return res.status(201).send("Guest added successfully."); // Return a success message
   } catch (err) {
     console.error(err);
